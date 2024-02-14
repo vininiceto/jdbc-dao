8fecfc907d2f1613df8e8cd5c353c26b4a53118d
vininiceto <viniciusniceto@gmail.com>
2024-02-14 17:11:54 -0300

    adicionando conexão com jdbc

---
+++ src/db/dbException.java
@@ -0,0 +1,11 @@
+package db;
+
+public class dbException extends RuntimeException {
+
+	private static final long serialVersionUID = 1L;
+
+	public dbException (String message) {
+		super(message);
+	}
+	
+}
